082f854fad0692ecbff09916c6a3cad18c1a39ba
Genio22 <104478212+Genio22@users.noreply.github.com>
2025-01-04 21:40:48 +0600

    Update main.c

--- main.c
+++ main.c
@@ -146,25 +146,7 @@ void displayRooms()
     }
 }
 
-// int calculatePrice(Room *room, int stayDuration, int month) {
-//     int price = room->basePrice;
 
-//     if (isPeakSeason(month)) {
-//         price *= 1.2;
-//     }
-
-//     if (stayDuration > 7) {
-//         price *= 0.85;
-//     } else if (stayDuration < 3) {
-//         price *= 1.15;
-//     }
-
-//     if (strcmp(room->view, "Sea View") == 0) {
-//         price *= 1.2;
-//     }
-
-//     return price * stayDuration; 
-// }
 
 // By sAhAf🙂
 
