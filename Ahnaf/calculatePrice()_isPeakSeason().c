32655230cf8058d77edbd811265f516da590ec0d
Genio22 <104478212+Genio22@users.noreply.github.com>
2025-01-15 08:46:30 +0600

    Create calculatePrice()_isPeakSeason().c

---
+++ Ahnaf/calculatePrice()_isPeakSeason().c
@@ -0,0 +1,35 @@
+int calculatePrice(Room *room, int stayDuration, int month)
+{
+    int price = room->basePrice;
+
+    if (isPeakSeason(month))
+    {
+        price *= 1.2;
+    }
+
+    if (stayDuration > 7)
+    {
+        price *= 0.85;
+    }
+    else if (stayDuration < 3)
+    {
+        price *= 1.15;
+    }
+
+    // if (strcmp(room->view, "Sea View") == 0)
+    //{
+    //     price *= 1.2;
+    // }
+
+    return price * stayDuration;
+}
+
+int isPeakSeason(int month)
+{
+    // peak season months ->June (6), July (7), November (11), December (12)
+    if (month == 6 || month == 7 || month == 11 || month == 12)
+    {
+        return 1;
+    }
+    return 0;
+}
