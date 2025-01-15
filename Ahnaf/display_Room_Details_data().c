4fb5726ebaab4fb505835a622cdaa8a0713a1aee
Genio22 <104478212+Genio22@users.noreply.github.com>
2025-01-15 08:48:17 +0600

    Create display_Room_Details_data().c

---
+++ Ahnaf/display_Room_Details_data().c
@@ -0,0 +1,25 @@
+void display_Room_Details_data()
+{
+    FILE *file = fopen("Room_details.txt", "r");
+    if (file == NULL)
+    {
+        printf("Error: Could not open file for reading.\n");
+        return;
+    }
+
+    char line[256];
+    // printf("----------------------------------------------------------------------------------------\n");
+    // printf("| Room Number  | Floor | Type      | View      | AC Type      | Base Price | Occupied |\n");
+    // printf("----------------------------------------------------------------------------------------\n");
+
+    // line diya full 1 block array access
+    fgets(line, sizeof(line), file);
+
+    // Read and print the rest of the lines
+    while (fgets(line, sizeof(line), file))
+    {
+        printf("%s", line); // Print the line as is
+    }
+
+    fclose(file);
+}
