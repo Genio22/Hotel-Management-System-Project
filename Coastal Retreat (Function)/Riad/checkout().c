ecf4941ea1c8a5a2ea7d90e16b55dffc6891a75f
Genio22 <104478212+Genio22@users.noreply.github.com>
2025-01-18 11:06:36 +0600

    first commit

---
+++ Coastal Retreat (Function)/Riad/checkout().c
@@ -0,0 +1,22 @@
+void checkout()
+{
+    int roomNumber = 0;
+    printf("Enter Room number: ");
+    scanf("%d", &roomNumber);
+    for (int i = 0; i < roomCount; i++)
+    {
+        if (roomNumber == hotelRooms[i].roomNumber)
+        {
+            if (hotelRooms[i].isOccupied)
+            {
+                hotelRooms[i].isOccupied = 0;
+                printf("You have been succesfully checkout from Room %d!\n", hotelRooms[i].roomNumber);
+            }
+            else
+            {
+                printf("Room %d is already Unoccupied. So checkout is not possible.\n", hotelRooms[i].roomNumber);
+            }
+            break;
+        }
+    }
+} 
